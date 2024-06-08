f145724f9b90ad402e1b612ffe283e187357d5fc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-06-08 21:45:19 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/springactionsdeploy/SpringActionsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.springactionsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActionsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActionsDeployApplication.class, args);
+	}
+
+}
